bf25b21fdb5088fb1d8e1a0948cedef824553f03
massytak <75225037+massytak@users.noreply.github.com>
2021-02-18 21:35:30 +0100

    feature first iteration

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 console.log("i'm ready!");
 // Iteration 1: Names and Input
-
-
+let hacker1= "massi";
+console.log(`the driver's name is ${hacker1}.`);
+let hacker2= "lolod";
+console.log(`the navigator's name is ${hacker2}.`);
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
